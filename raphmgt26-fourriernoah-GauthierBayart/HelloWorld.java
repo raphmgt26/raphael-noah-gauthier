f825a4628cdf1a6766ad65473275d90d6c8f41b5
Noah Fourrier <fourriernoah>
2026-03-30 14:41:05 +0200

    test

--- raphmgt26-fourriernoah-GauthierBayart/HelloWorld.java
+++ raphmgt26-fourriernoah-GauthierBayart/HelloWorld.java
@@ -1,1 +1,5 @@
-﻿public class HelloWorld { public static void main(String[] args) { System.out.println("Hello BUT1 Info!"); } }
+﻿public class HelloWorld { 
+    public static void main(String[] args) { 
+        System.out.println("Hello BUT1 Info!"); 
+    } 
+}
